469bd67498ead5f564e2494adc638cb8016b2cff
siverNa <dhrru123@naver.com>
2020-08-25 21:05:10 +0900

    Create basic_form.php
    이제부터 만들어볼 php 연습

---
+++ basic_form.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=euc-kr"/>
+</head>
+    <body>
+        <h3>html 및 php 기본 폼</h3>
+        
+        
+    </body>
+</html>
