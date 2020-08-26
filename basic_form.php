151621ef1d6291ee01d7fba4971502b571cc1698
siverNa <dhrru123@naver.com>
2020-08-26 16:13:41 +0900

    add php study file
    생활코딩 url 공부 추가.
    
    add url study file

--- basic_form.php
+++ basic_form.php
@@ -1,6 +1,7 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=euc-kr"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </head>
     <body>
         <h3>html 및 php 기본 폼</h3>
---
+++ study/url_input.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+    <body>
+        <h3>url 입력 연습이에요</h3>
+            <p> 안녕하세요
+            <?php echo $_GET['address']; ?> 에 사시는
+            <?php echo $_GET['name']; ?> 님!
+            </p>
+        
+    </body>
+</html>
