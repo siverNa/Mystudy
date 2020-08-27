38bed535a9776a3d4173e18255d384adca7fe92c
siverNa <dhrru123@naver.com>
2020-08-27 18:48:21 +0900

    add php study file
    생활코딩 php 반복문, 제어문, 배열 항목을 이용해 공부한 것
    
    20.08.27

--- study/index.php
+++ study/index.php
@@ -4,17 +4,42 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 </head>
     <body>
-        <h3>WEB</h3>
+        <h3><a href="index.php">WEB</a></h3>
         <ol>
-            <li><a href="index.php?id=html">HTML</a></li>
-            <li><a href="index.php?id=css">CSS</a></li>
-            <li><a href="index.php?id=javascript">JavaScript</a></li>
+            <?php
+                $list = scandir('./data');
+                
+                $i = 0;
+                while($i < count($list)){
+                    if($list[$i] != "."){
+                        if($list[$i] != ".."){
+                            echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+                        }
+                    }
+                    $i += 1;
+                }
+                
+            ?>
         </ol>
         <h2>
-            <?php echo $_GET['id']; ?>
+            <?php
+                if(isset($_GET['id'])){
+                    echo $_GET['id']; 
+                } 
+                else{
+                    echo "Welcome";
+                }
+            ?>
         </h2>
         <p>
-            <?php echo file_get_contents("data/".$_GET['id']); ?>
+            <?php 
+                if(isset($_GET['id'])){
+                    echo file_get_contents("data/".$_GET['id']); 
+                }
+                else{
+                    echo "Hello, PHP";
+                }
+            ?>
         </p>
     </body>
 </html>
