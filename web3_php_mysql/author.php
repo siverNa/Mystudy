e740d9a6d230119a48b700a77f281ab916d0b360
siverNa <69504543+siverNa@users.noreply.github.com>
2020-09-10 22:21:02 +0900

    add php,mysql study file
    author 테이블에 업데이트 버튼 생성 및
    업데이트에 필요한 변수, 명령 생성,
    update_process_author.php 생성
    
    20.09.10

--- web3_php_mysql/author.php
+++ web3_php_mysql/author.php
@@ -13,7 +13,7 @@
     <p><a href="index.php">topic</a></p>
     <table border="1">
         <tr> <!-- table 에서 tr은 행, td는 열을 나타냄, 하나의 tr은 하나의 행, 하나의 td는 하나의 열임. -->
-            <td>id</td><td>name</td><td>profile</td>
+            <td>id</td><td>name</td><td>profile</td><td></td>
             <?php
                 $sql = "SELECT * FROM author";
                 $result = mysqli_query($conn, $sql);
@@ -29,16 +29,43 @@
                         <td><?=$filtered['id']?></td>
                         <td><?=$filtered['name']?></td>
                         <td><?=$filtered['profile']?></td>
+                        <td><a href="author.php?id=<?=$filtered['id'] ?>">update</a></td>
                     </tr>
                     <?php
                 }
             ?>
         </tr>
     </table>
-    <form action="create_process_author.php" method="post">
-        <p><input type="text" name="name" placeholder="name"></p>
-        <p><textarea name="profile" cols="30" rows="10" placeholder="profile"></textarea></p>
-        <p><input type="submit" value="Create Author"></p>
+    <?php
+        $escaped = array( //id 값이 없는 경우에는 기본 텍스트가 표시되도록 설정
+            'name' => '',
+            'profile' => ''
+        );
+
+        $lable_submit = 'Create Author'; //update 버튼을 누르기 전엔 생성 버튼 텍스트를 표시함
+        $form_action = 'create_process_author.php'; //update 버튼을 누르기 전엔 생성프로세스를 수행
+        $form_id = ''; //update_process_author.php 에 id 값을 넘겨주기 위한 변수, update 버튼을 누르기 전엔 아무 값도 없음
+
+        if(isset($_GET['id'])){ //update 버튼을 눌렀을 경우 
+            $filtered_id = mysqli_real_escape_string($conn, $_GET['id']);
+            settype($filtered_id, 'integer');
+            $sql = "SELECT * FROM author WHERE id={$filtered_id} ";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_array($result);
+
+            $escaped['name'] = htmlspecialchars($row['name']);
+            $escaped['profile'] = htmlspecialchars($row['profile']);
+
+            $lable_submit = 'Update Author';
+            $form_action = 'update_process_author.php';
+            $form_id = '<input type="hidden" name="id" value="'.$_GET['id'].'" ';
+        }       
+    ?>
+    <form action="<?=$form_action?>" method="post">
+        <?=$form_id?>
+        <p><input type="text" name="name" placeholder="name" value="<?=$escaped['name'] ?>"></p>
+        <p><textarea name="profile" cols="30" rows="10" placeholder="profile"><?=$escaped['profile']?></textarea></p>
+        <p><input type="submit" value="<?=$lable_submit?>"></p>
     </form>
   </body>
 </html>
---
+++ web3_php_mysql/update_process_author.php
@@ -0,0 +1,46 @@
+<?php
+    $conn = mysqli_connect('localhost', 'root', '111111', 'opentutorials'); //mysqli_connect() : mysql과 php를 연결하는 함수
+
+    settype($_POST['id'], 'integer'); //넘어온 id의 값이 무조건 정수여야하므로 정수로 형변환('settype()'함수 사용)
+
+    $filtered = array(
+        'id' => mysqli_real_escape_string($conn, $_POST['id']),
+        'name' => mysqli_real_escape_string($conn, $_POST['name']),
+        'profile' => mysqli_real_escape_string($conn, $_POST['profile'])
+        //mysqli_real_escape_string() : SQL injection 공격을 방어하기 위한 함수. 인자로 들어온 데이터 중에서 sql injection 공격과
+        //관련된 여러가지 기호를 문자로 바꿔버리는 함수.
+    );
+
+    $sql = "
+        UPDATE author
+            SET
+                name = '{$filtered['name']}',
+                profile = '{$filtered['profile']}'
+            WHERE 
+                id = '{$filtered['id']}'
+    ";
+
+    $result = mysqli_query($conn, $sql); //mysqli_query() : 쿼리문을 연결할 주소와 같이 담아 전송, true 또는 false로 반환하는 함수.
+
+    if($result == false){
+        echo "수정하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.";
+        error_log(mysqli_error($conn)); 
+        /*mysqli_error() : 에러가 있을 경우 에러 정보를 알려주는 함수
+        error_log() : 에러 로그를 아파치 error 로그파일에 저장하는 함수.*/
+    } else {
+        header('Location: author.php?id='.$filtered['id']);
+    }
+
+    mysqli_close($conn); //mysqli_close() : mysql과 연결을 끊는 함수
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
